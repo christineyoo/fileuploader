da8e950366202a2d504fbb0528c9919810c70654
Christine Yoo <christinecho.s@gmail.com>
2021-02-15 20:08:13 -0800

    added interactivity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar/SearchBar";
 import FilterableList from "./FilterableList/FilterableList";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      filterOption: "All",
+    };
+  }
+
+  updateSearchTerm(term) {
+    this.setState({ searchTerm: term });
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <FilterableList files={this.props.files} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOption}
+          handleUpdate={(term) => this.updateSearchTerm(term)}
+          handleFilterChange={(option) => this.updateFilterOption(option)}
+        />
+        <FilterableList
+          files={this.props.files}
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOption}
+        />
       </div>
     );
   }
--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -8,7 +8,11 @@ class SearchBox extends Component {
     return (
       <div className="SearchBox">
         <FontAwesomeIcon icon={faSearch} />
-        <input placeholder="Search term" />
+        <input
+          value={this.props.searchTerm}
+          placeholder="Search term"
+          onChange={(e) => this.props.handleUpdate(e.target.value)}
+        />
       </div>
     );
   }
